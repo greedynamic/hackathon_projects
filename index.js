e924b0313311b8bb454465c7f50c719a8b30772f
Alex <yuchun.li4096@gmail.com>
2021-02-17 04:55:14 -0800

    add base code of Battlesnake

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+// var pool;
+// pool = new Pool({
+//   connectionString: process.env.DATABASE_URL
+// });
+//
+// pool.connect();
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.urlencoded({extended: false}))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req,res) => res.render('pages/homepage'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+/*
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
